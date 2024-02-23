e3468a19481df7c8fc51ca138b73a62fe049b763
Fernando Crozetta <fernando@czetta.com>
2024-02-23 01:24:21 +0100

    typo?

--- Formula/fc-json.rb
+++ Formula/fc-json.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class fcJson < Formula
+class FcJson < Formula
   desc "Personal tools to make my life easier"
   homepage ""
   url "https://github.com/fcrozetta/toolkit/releases/download/latest/toolkit.zip"
